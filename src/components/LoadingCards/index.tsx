6c9f79c443b29aca42d3e9dabf35ea30f08109bd
Gustavo Silva <gustavoalprofissional@gmail.com>
2021-06-04 19:19:02 -0300

    :sparkles: adding loading state in recipes page

---
+++ src/components/LoadingCards/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from './styled';
+
+const LoadingCards = () => {
+  return (
+    <>
+      <Container />
+      <Container />
+      <Container />
+    </>
+  );
+};
+
+export default LoadingCards;
---
+++ src/components/LoadingCards/styled.ts
@@ -0,0 +1,36 @@
+import styled, { keyframes } from 'styled-components';
+
+const shimmer = keyframes`
+  0% {
+    background-position: -468px 0;
+  }
+  100% {
+    background-position: 468px 0;
+  }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  border: 1px solid var(--gray-200);
+  height: 200px;
+  border-radius: 5px;
+
+  background: #f6f7f8;
+  background-image: linear-gradient(
+    to right,
+    #f6f7f8 0%,
+    #edeef1 20%,
+    #f6f7f8 40%,
+    #f6f7f8 100%
+  );
+  background-repeat: no-repeat;
+  background-size: 100% 200px;
+  display: inline-block;
+  position: relative;
+
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  animation-name: ${shimmer};
+  animation-timing-function: linear;
+`;
